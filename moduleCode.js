f79122cb661a5425bd9d353fc8c2d99f5e9d6a79
mpvkarthick <mpvkarthick@gmail.com>
2018-02-20 23:29:57 -0800

    initial commit
    Sample Program to Define Modules and How to export

---
+++ moduleCode.js
@@ -0,0 +1,20 @@
+
+// Define Module 1
+var externalModule1 = function(){
+    console.log("Hello External Module 1");
+}
+
+// Define Module 2
+var externalModule2 =  function(){
+    console.log("Hello External Module 2");
+}
+
+// Below code makes sure the modules are exported to outside of this script
+module.exports = {
+    externalModule1, externalModule2
+}
+
+/*
+If there is only one module then
+module.exports = externalModule1;
+*/
